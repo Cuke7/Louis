f84d7c4e7d2def69ddc81cfa25095a0746fff58c
Cuke7 <cuke.dev@mail.com>
2021-10-14 17:46:25 +0200

    Auditere

---
+++ components/Auditere.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-row align="center" class="pa-4">
+    <v-col order="2" order-md="1" cols="12" md="6" class="text-center">
+      <v-img contain max-height="500" src="screenshot_auditere.png"></v-img>
+    </v-col>
+    <v-col order="1" order-md="2" cols="12" md="6" align-self="start">
+      <v-row align="center">
+        <v-col cols="auto">
+          <div class="text-h2">
+            Auditere
+          </div>
+        </v-col>
+        <v-col cols="auto">
+          <v-img width="70" src="icon_auditere.png"></v-img>
+        </v-col>
+      </v-row>
+
+      <div class="text-md-left text-center">
+        <v-btn
+          color="#0083E5"
+          class="mt-12"
+          href="https://auditere.netlify.app"
+          target="_blank"
+        >
+          Essayer Auditere
+        </v-btn>
+      </div>
+
+      <div class="text-body-1 mt-12 text-justify">
+        Permet de rechercher et lire de la musique sur Youtube tout en
+        vérouillant le téléphone. Peut aussi enregistrer et lire des playlists
+        crées sur Youtube.
+      </div>
+    </v-col>
+  </v-row>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Auditere></Auditere>
+  </div>
+</template>
+
+<style>
+.v-main {
+  background-image: linear-gradient(
+    to right top,
+    #3a46c9,
+    #0072e1,
+    #008ed2,
+    #00a3ad,
+    #00b389
+  );
+}
+</style>
